6a8bce1b1e0ce635c38fcfb942cbfd38b8af3775
Ben Carlson <bjcarlson42@outlook.com>
2019-06-06 11:37:35 -0400

    Created initial project

---
+++ MM Kennels/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MM_Kennels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
