74be19792e308d56b70bcf5b0edb6b6e2aaa140c
Ben Carlson <bjcarlson42@outlook.com>
2019-06-10 11:57:17 -0400

    Text Files

--- MM Kennels/Program.cs
+++ MM Kennels/Program.cs
@@ -11,38 +11,58 @@ namespace MM_Kennels
             List<Cage> cages = new List<Cage>();
             List<Animal> animals = new List<Animal>();
 
-            StreamReader sr = new StreamReader(args[1]);
-            string text = sr.ReadLine();
-            foreach (char t in text)
-            {
-                int min = int.Parse(args[1]);
-                int max = int.Parse(args[2])
+            //StreamReader sr = new StreamReader(args[1]);
+            //string text = sr.ReadLine();
+            //foreach (char t in text)
+            //{
+            //    int min = int.Parse(args[1]);
+            //    int max = int.Parse(args[2]);
+            //    int ds = 0; //ds = day scheduled
 
-                cages.Add(new Cage(min, max, 45, true));
-            }
+            //    cages.Add(new Cage(min, max, ds, false));
+            //}
+
+            Console.WriteLine("Loading cages...");
+
             foreach (Cage c in cages)
             {
                 Console.WriteLine(c);
             }
 
-            if (args[0].ToLower() == "schedule")
+            bool run = true;
+            while (run)
             {
-                //Schedule(args[1], args[2], args[3], args[4]);
-            }
-            else if (args[0].ToLower() == "day")
-            {
+                if (args[0].ToLower() == "schedule")
+                {
+                    //Schedule(args[1], args[2], args[3], args[4]);
+                }
+                else if (args[0].ToLower() == "day")
+                {
 
-            }
-            else if (args[0].ToLower() == "animal")
-            {
-                //IsScheduled(args[0], args);
-                Console.WriteLine(IsScheduled(args[1], args));
+                }
+                else if (args[0].ToLower() == "animal")
+                {
+                    //IsScheduled(args[0], args);
+                    Console.WriteLine(IsScheduled(args[1], args));
 
-            }
-            else
-            {
-                Console.WriteLine("Please enter a valid command.");
-                Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid command.");
+                    Console.ReadKey();
+                }
+
+                Console.WriteLine("Do you want to enter another command? Enter \"y\" or \"n\" and press enter");
+                Console.ReadLine();
+                if(Console.ReadLine() == "y")
+                {
+                    run = true;
+                    Console.WriteLine("Enter Command...");
+                }
+                else
+                {
+                    run = false;
+                }
             }
         }
 
