705303a1f8b234f6e4a12d6319cbd3b39649f9f8
Ben Carlson <bjcarlson42@outlook.com>
2019-06-26 11:08:02 -0400

    Docker fully implented.

--- MM Kennels/Startup.cs
+++ MM Kennels/Startup.cs
@@ -52,10 +52,10 @@ namespace MM_Kennels
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
